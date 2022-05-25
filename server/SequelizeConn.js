02789ef31839c4c5a93c14016f1b23839acc86de
kiattisakng9 <kiattisakng9@gmail.com>
2022-05-25 23:07:57 +0700

    Initialize sequelize on the backend app

---
+++ server/SequelizeConn.js
@@ -0,0 +1,36 @@
+const { Sequelize } = require("sequelize");
+const dayjs = require("dayjs");
+const fs = require("fs");
+
+const seqHost = process.env.PG_HOST;
+const seqDialect = process.env.PG_DIALECT;
+const seqDB = process.env.PG_DB;
+const seqUsername = process.env.PG_USERNAME;
+const seqPassword = process.env.PG_PASSWORD;
+
+// Create an instance of Sequelize
+const sequelizeInstance = new Sequelize(seqHost, seqDialect, seqDB, {
+  host: seqUsername,
+  dialect: seqPassword,
+});
+
+// Initialize Sequelize connection
+sequelizeInstance.authenticate().catch((err) => {
+  console.error("Unable to connect to the database:", err.message);
+  logMessage(err);
+});
+
+// Function to log error message
+const logMessage = (error) => {
+  const datetime = dayjs();
+
+  const errorMessage = error?.message != undefined ? error?.message : "ERROR!";
+
+  const errorEntry = `[${datetime}] - ${errorMessage}\n`;
+
+  fs.appendFile("./log.txt", errorEntry, (err) => {
+    if (err) console.log("Unable to log message");
+  });
+};
+
+module.exports = sequelizeInstance;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-
+const { sequelize, users } = require("./models");
 const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("All good!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`);
+  await sequelize.authenticate();
+  console.log("Database Connected!");
 });
