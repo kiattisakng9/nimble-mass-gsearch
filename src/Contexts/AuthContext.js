7500bf87c53ec099ea4d7b1c9d7021dabe2cf1bf
kiattisakng9 <kiattisakng9@gmail.com>
2022-05-28 15:59:58 +0700

    Build user registration function

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -20,6 +20,29 @@ const useProvideAuth = () => {
     }
   }, [sessionUser]);
 
+  // Register a new user function
+  const registerUser = async (params) => {
+    const registrationResults = await Axios.post("/users", params)
+      .then((res) => res.data)
+      .catch((err) => {
+        console.log("err :", err);
+        alert("something went wrong");
+      });
+
+    // Errors returned
+    const resultErrors = registrationResults?.error ?? null;
+    const hasErrors = resultErrors != null;
+
+    // Display error message if errors are returned
+    if (hasErrors) {
+      const errorMessage = getErrorMessages(resultErrors);
+
+      const errorBody = `Errors detected!\n ${errorMessage}`;
+
+      alert(errorBody);
+    } else alert("User is created successfully!");
+  };
+
   // Sign user in function
   const signin = async (credentials) => {
     const authResult = await Axios.post("/auth/login", credentials)
@@ -29,25 +52,39 @@ const useProvideAuth = () => {
         alert("something went wrong");
       });
 
+    // Errors returned
     const resultIsValid = authResult != null;
     const isAuthenticated = resultIsValid ? authResult.authenticated : false;
 
+    // Display error message if errors are returned
     if (isAuthenticated) {
       const returnedUser = authResult.user;
 
       const stringifiedUser = JSON.stringify(returnedUser);
+
+      // Set user in context and browser session
       sessionStorage.setItem("user", stringifiedUser);
-      setUser(credentials);
+      setUser(returnedUser);
     } else alert("Incorect login credentials");
   };
 
   // Sign user out function
   const signout = async () => {
+    // Remove user from context and browser session
     setUser(null);
     sessionStorage.removeItem("user");
   };
 
-  return { user, signin, signout };
+  // Build error messages from error returned
+  const getErrorMessages = (errors) => {
+    const errorParamMessage = errors.map(
+      (error) => `* ${error.param} - ${error.msg}\n`
+    );
+
+    return errorParamMessage;
+  };
+
+  return { user, registerUser, signin, signout };
 };
 
 // Authenticator context provider
--- src/Pages/RegistrationPage.js
+++ src/Pages/RegistrationPage.js
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useAuth } from "../Contexts/AuthContext";
 
 const RegistrationPage = () => {
-  return <div>RegistrationPage</div>;
+  const auth = useAuth();
+
+  // References to form inputs
+  const inputFirstName = useRef("");
+  const inputLastName = useRef("");
+  const inputEmail = useRef("");
+  const inputPassword = useRef("");
+
+  // Update email reference on input change
+  const handleOnChangeInputs = (evt, reference) => {
+    const value = evt.target.value;
+
+    // Update value
+    reference.current = value;
+  };
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Trim values
+    const trimmedFirstName = inputFirstName.current.trim();
+    const trimmedLastName = inputLastName.current.trim();
+    const trimmedEmail = inputEmail.current.trim();
+    const trimmedPassword = inputPassword.current.trim();
+
+    const newUserInfo = {
+      first_name: trimmedFirstName,
+      last_name: trimmedLastName,
+      email: trimmedEmail,
+      password: trimmedPassword,
+    };
+
+    auth.registerUser(newUserInfo);
+  };
+
+  return (
+    <div>
+      <Row>
+        <Container className='App'>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className='mb-3' controlId='firstName'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                required
+                type='text'
+                name='firstName'
+                placeholder='Enter first name'
+                onChange={(evt) => handleOnChangeInputs(evt, inputFirstName)}
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='lastName'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                required
+                type='text'
+                name='lastName'
+                placeholder='Enter last name'
+                onChange={(evt) => handleOnChangeInputs(evt, inputLastName)}
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='email'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                required
+                type='email'
+                name='email'
+                placeholder='Enter email'
+                onChange={(evt) => handleOnChangeInputs(evt, inputEmail)}
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type='password'
+                name='password'
+                placeholder='Password'
+                minLength={8}
+                onChange={(evt) => handleOnChangeInputs(evt, inputPassword)}
+              />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </Row>
+    </div>
+  );
 };
 
 export default RegistrationPage;
