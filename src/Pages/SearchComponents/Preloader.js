393351e9ad2195eae49d4df09fa1f353c0eae267
kiattisakng9 <kiattisakng9@gmail.com>
2022-05-30 19:41:29 +0700

    Add a preloader animation component

---
+++ src/Pages/SearchComponents/Preloader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import loader from "./../../Images/preloader.gif";
+
+const Preloader = () => {
+  return <img id='preloader' src={loader} alt='preloader' />;
+};
+
+export default Preloader;
